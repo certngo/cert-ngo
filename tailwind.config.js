e1c3fe7ef6e8c37ba497de84392fc018b8ca9075
Tomasz.Pisarewski <tomasz.pisarewski@crealogix.com>
2022-03-08 22:09:10 +0100

    Update index.html

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+  
+    mode: 'jit',
+    purge: [
+      './index.html',
+      './style.css',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+        colors: {
+          '4sektor': '#1e212b',
+          'greenish': '#00cc33',
+          'grayish': '#626c78',
+          'box':'#626c78',
+        },
+        fontFamily:{
+          lato: ['Lato', 'sans-serif']
+        }
+      }
+    },
+    variants: {
+      extend: {}
+    }
+  };
